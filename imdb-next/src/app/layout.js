93ab7d900e9fe6329d343500657f3f76c55ea5f7
unknown <Hp@EPEY.csph-cameroun.org>
2024-08-11 12:40:23 +0100

    added dark and light mode function

--- imdb-next/src/app/layout.js
+++ imdb-next/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Providers from "./Providers";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,9 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Headers /> */}
-        <Header />
-        {children}
+        <Providers>
+          {/*  */}
+          {/* <Headers /> */}
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );

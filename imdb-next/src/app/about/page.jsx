ae62f76c86499d54e10afce09d1cb860a19448ce
unknown <Hp@EPEY.csph-cameroun.org>
2024-08-11 13:43:32 +0100

    add about page

---
+++ imdb-next/src/app/about/page.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tristique porttitor sapien, 
+        ut dictum libero vestibulum non. Donec eget aliquam elit, tincidunt ullamcorper velit. 
+        Nunc suscipit dui eu rutrum scelerisque. Etiam efficitur sapien nec sollicitudin lobortis. 
+        Duis dictum nulla feugiat vestibulum egestas. Vestibulum fermentum eu orci in rutrum. Sed nisi sapien, 
+        elementum eget bibendum eget, tincidunt at mi. Proin vel felis magna. Donec fringilla pretium volutpat. 
+        Praesent elit enim, convallis tincidunt porta sit amet, eleifend eget felis. Cras dictum ac eros at eleifend.
+         Fusce malesuada magna sed diam posuere, vitae scelerisque velit fermentum. Vestibulum varius mi vitae ultricies 
+         eleifend. Quisque nec eros mollis, finibus nibh eu, mollis turpis.
+        </p>
+        <p>
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse nec diam pharetra ipsum molestie imperdiet. Ut scelerisque tellus odio, sed sollicitudin ex porta iaculis. Sed nec augue sit amet leo consectetur porta quis id leo. Mauris est est, tincidunt sed justo et, interdum suscipit urna. Quisque at ex a mi convallis maximus. Donec commodo justo ut ligula dapibus, ut imperdiet sapien pellentesque. Suspendisse bibendum nisi sed augue pulvinar aliquam. Nunc dignissim est at laoreet hendrerit. Nam sit amet erat purus. Nulla facilisi.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tristique porttitor sapien, 
+        ut dictum libero vestibulum non. Donec eget aliquam elit, tincidunt ullamcorper velit. 
+        Nunc suscipit dui eu rutrum scelerisque. Etiam efficitur sapien nec sollicitudin lobortis. 
+        Duis dictum nulla feugiat vestibulum egestas. Vestibulum fermentum eu orci in rutrum. Sed nisi sapien, 
+        elementum eget bibendum eget, tincidunt at mi. Proin vel felis magna. Donec fringilla pretium volutpat. 
+        Praesent elit enim, convallis tincidunt porta sit amet, eleifend eget felis. Cras dictum ac eros at eleifend.
+         Fusce malesuada magna sed diam posuere, vitae scelerisque velit fermentum. Vestibulum varius mi vitae ultricies 
+         eleifend. Quisque nec eros mollis, finibus nibh eu, mollis turpis.
+        </p>
+    </div>
+  )
+}

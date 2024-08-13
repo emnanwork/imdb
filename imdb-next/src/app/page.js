13cabc4eb77cfda53a26ef69253a62b8a9b5f398
unknown <Hp@EPEY.csph-cameroun.org>
2024-08-13 08:01:17 +0100

    getting result from themoviedb api

--- imdb-next/src/app/page.js
+++ imdb-next/src/app/page.js
@@ -1,9 +1,49 @@
-import Image from "next/image";
+import Results from "@/components/Results";
 
-export default function Home() {
+
+const API_KEY = process.env.API_KEY;
+
+
+export default async function Home({searchParams}) {
+  // 
+  const genre = searchParams.genre || 'fetchTrending';
+  const url = `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+    }?api_key=${API_KEY}&language=en-US&page=1`;
+    console.log(url);
+  const res = await fetch(url);
+  // const res = await fetch(
+  //   `https://api.themoviedb.org/3${
+  //     genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+  //   }?api_key=${API_KEY}&language=en-US&page=1`
+  // );
+  // // 
+  const data = await res.json();
+  // 
+  if(!res.ok) {
+    throw new Error('Failed to fetch data')
+  } else {
+    // 
+    const results = data.results;
+    // console.log(results);
+  }
+  const results = data.results;
+
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWJhYTBjNGM1ZjAxMGRlOThlMTBiOGViNTAwMzgyMCIsIm5iZiI6MTcyMzQ5NTI4My41MzA3Mywic3ViIjoiNjZiYTViODIzODQwYjE0Njk4MDVmZWQxIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.dOYGB5rcrftq70C-aVUdELt57-IAKRO8XXIRRxQK49c'
+  //   }
+  // };
+  // fetch('https://api.themoviedb.org/3/movie/changes?page=1', options)
+  // .then(response => response.json())
+  // .then(response => console.log(response))
+  // .catch(err => console.error(err));
+
   return (
     <div>
-      Home
+      <Results  results={results}/>
     </div>
   
   );

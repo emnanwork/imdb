92434024872e9c34c85954964877b5b0779831ea
unknown <Hp@EPEY.csph-cameroun.org>
2024-08-15 09:39:50 +0100

    add loading effect

--- imdb-next/src/app/page.js
+++ imdb-next/src/app/page.js
@@ -1,4 +1,5 @@
 import Results from "@/components/Results";
+import { resolve } from "styled-jsx/css";
 
 
 const API_KEY = process.env.API_KEY;
@@ -10,8 +11,22 @@ export default async function Home({searchParams}) {
   const url = `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
     }?api_key=${API_KEY}&language=en-US&page=1`;
-    console.log(url);
-  const res = await fetch(url);
+  //   console.log(url);
+  // const res = await new Promise((resolve) => {
+  //   // 
+  //   setTimeout(async () => {
+  //     // 
+  //     const response = await fetch(
+  //       url,
+  //       {next: {revalidate:60}}
+  //     );
+  //     resolve(response);
+  //   }, 2000); //delay of two second
+  // });
+  const res = await fetch(
+    url,
+    {next: {revalidate:10000}}
+  );
   // const res = await fetch(
   //   `https://api.themoviedb.org/3${
   //     genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
